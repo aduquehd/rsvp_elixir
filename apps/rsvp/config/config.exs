6ae55fd89053822b6b73c85206f5142eec4b41d1
Unknown <saduqz@gmail.com>
2018-07-24 07:06:45 -0500

    Fixed local db port and password

--- apps/rsvp/config/config.exs
+++ apps/rsvp/config/config.exs
@@ -33,8 +33,8 @@ config :rsvp, Rsvp.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "rsvp",
   username: "postgres",
-  password: "abcd1234.",
+  password: "abcd1234....",
   hostname: "localhost",
-  port: "5433"
+  port: "5432"
 
 config :rsvp, ecto_repos: [Rsvp.Repo]
